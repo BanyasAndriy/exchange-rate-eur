ffe09463453ac1ee438dfb7a4b635a946e097801
AndriyBanyas <andriy_kross_ua@ukr.net>
2020-02-05 15:14:33 +0200

    up5

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,15 +3,16 @@ $(document).ready(function(){
     $.getJSON( "/rate={rate}" , function(data) {
 
 
-        let url = window.location.href.substr(28);
+        let url = window.location.href;
 
+        let toRate=url.substr(url.length-3);
 
         let rate;
         let some;
 
         $('#rate').text(data.exchangeRate);
         $('#date').text(data.date);
-        $('#valute').text(url.toUpperCase());
+        $('#valute').text(toRate.toUpperCase());
 
 
 
